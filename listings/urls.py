75e2eb4e67b8a62b48aa5afc9597a2ce652a5f03
JayHChoi <jaychoi1619@gmail.com>
2018-11-04 07:37:21 +0700

    ListingListView render

--- listings/urls.py
+++ listings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='listings'),
-    path('<int:listing_id>/', views.listing, name='listing'),
+    path('', views.ListingListView.as_view(), name='listings'),
+    path('<int:id>/', views.listing, name='listing'),
     path('search/', views.search, name='search'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Listing
 
-def index(request):
-    return render(request, 'listings/listings.html')
 
-def listing(request):
+class ListingListView(generic.ListView):
+    model = Listing
+    template_name = 'listings/listings.html'
+
+# def index(request):
+#     return render(request, 'listings/listings.html')
+
+def listing(request, id):
     return render(request, 'listings/listing.html')
 
 def search(request):
